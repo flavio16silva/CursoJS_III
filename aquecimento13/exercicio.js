0378c732ecd952a1f932894a258b6809b7434815
Flavio Silva <fal_silva@hotmail.com>
2023-08-30 15:31:18 -0300

    novas anotações

--- aquecimento13/exercicio.js
+++ aquecimento13/exercicio.js
@@ -8,28 +8,32 @@ const obj = {
   prop3: [1, 2, 3]   //esse array é um objeto aninhado no objeto inicial
 }
 
-// const obj2 = {
-//   ...obj,                                 //propriedades de obj foram espalhadas ali dentro - Spread
-//   prop: 'x',                              // alterou a propriedade de prop
-//   prop3: [...obj.prop3, 4]                //False, pq são dois arrays diferentes em memoria
-// }
+const obj2 = {
+   ...obj,                                 //propriedades de obj foram espalhadas ali dentro - Spread
+   prop: 'x',                              // alterou a propriedade de prop
+   prop3: [...obj.prop3, 4]                //False, pq são dois arrays diferentes em memoria
+  //Para copiar objetos aninhados: Primeiro: Subscrevendo a prop3 que foi espalhada, criando um novo array. 
+  //Segundo:são criadas cópias do obj nesse novo array
+  //Na linha14 o numero 4 ao ser incluido só irá aparecer no novo array criado, ou seja, na cópia que foi criada. 
+}
 
-//obj2.prop3.push(4)
-const obj2 = structuredClone(obj)            //Retorna: false, 2 arrays diferentes em memoria.
+//obj2.prop3.push(4)                             //pelo metodo push o 4 apareceu no array, porque objetos são tipos passados por referência
+//const obj2 = structuredClone(obj)              //Retorna: false, 2 arrays diferentes em memoria. Necessário comentar as linhas12,13,14
 
-console.log('prop3 do obj2:', obj2.prop3)
 console.log('prop3 do obj:', obj.prop3)
-console.log(obj.prop3 === obj2.prop3)         //Estao armazenando o mesmo array em memoria: true
+console.log('prop3 do obj2:', obj2.prop3)
+console.log(obj.prop3 === obj2.prop3)         //Ao descomentar a linha14, retorna FALSE, porque são 2 arrays diferentes em memoria.
+                                              //Estao armazenando o mesmo array em memoria: true
 
-//Cuidado ao usar structuredClone
+//Cuidado ao usar structuredClone:
 //Será que seu objeto precisa ter todas as propriedades do objeto original.
-//Melhor: cria um clone somente com as propriedades que serão usadas e não todas as propriedades do objeto. 
+//Melhor: cria um clone d oobjeto somente com as propriedades que serão usadas e não todas as propriedades do objeto. 
 
 
 
 //============= Destructuring Assignment
 //expressao que possibilida a atribuição de valores de um objeto de forma mais concisa/simples. 
-//Atribução com desistruração
+//Atribução com desistruturação
 //Um dos problemas que são resolvidos de interpolação com uso de Destructuring
 
 const playlist = {
@@ -40,6 +44,7 @@ const playlist = {
 }
 
 // =============== Usando:
+console.log('=== Usando Destructuring ======= ')
 const {nome, videos, visualizacoes, visibilidade } = playlist    // a abertura e fechamento de chaves a esquerda do sinal de atribição é Destructuring
 //Tudo que encontra-se dentro da chave absorveu o valor da propriedade do objeto acima: playlist
 // ======== Com Destructuring
@@ -68,51 +73,52 @@ const config = {
   }
 }
 // =============== Se o tipo em questão é objeto, ele não faz copia apenas referencia
-// const apanhaMensagem = ({futuro}) => `v2Meta é true? ${futuro.v2Meta === true } ` //futuro é um objeto, somente uma referencia para o objeto principal
-// const mens = apanhaMensagem(config) //objeto passado como argumento
-// console.log(mens) 
+const apanhaMensagem = ({futuro}) => `v2Meta é true? ${futuro.v2Meta === true } ` //futuro é um objeto, somente uma referencia para o objeto principal
+const mens = apanhaMensagem(config) //objeto passado como argumento
+console.log(mens) 
 
-const apanhaMensagem = ({futuro}) => {
-    return {...futuro, v2Meta: false}             //criando novo objeto já com a propriedade desejada.
+//const apanhaMensagem = ({futuro}) => {
+    //return {...futuro, v2Meta: false}             //criando novo objeto já com a propriedade desejada.(Não modificar o objeto principal)
+    //futuro.v2Meta = false                         //Modificando o objeto original, isso é errado nesse caso
   //`v2Meta é true? ${futuro.v2Meta === true } `
-} 
+//} 
 
 
-const mens = apanhaMensagem(config)
-console.log(config.futuro.v2Meta)
-console.log(mens.v2Meta) //armazena falso que foi o valor setado dentro do return
+//const mens = apanhaMensagem(config)
+console.log(config.futuro.v2Meta)                 //false, ou seja alterou objeto principal
+console.log(mens.v2Meta)                          //armazena false, que foi o valor setado dentro do return
 
 
 // ============= Quando seu Destructuring, fica ilegivel: EVITAR
 const pegueMensagem = ({futuro: {v2Meta}}) => `v2Meta é true? ${v2Meta === true } ` //Evitar faz aninhamento de Destructuring
 const m = pegueMensagem(config)
-console.log(m) 
+console.log('m:',m) 
 
 
 //Qual o problema que Destructuring resolve em ARRAY
 //========================== EXEMPLO 1 ================================
 const arr = ['Joao', 'Silva']
-const n = arr[0]
+const n = arr[0]               //o problema é o uso da sintaxe usado nos dois casos, para obter o nome do array. arr[0]...
 const s = arr[1]
 
 const nomeCompleto = `${n} ${s}`
 
-console.log(n)
-console.log(s)
-console.log(nomeCompleto)
+console.log('n:', n)
+console.log('s:', s)
+console.log('nomeCompleto:', nomeCompleto)
 
 //========================== EXEMPLO 2 ================================
 const array = [5, num => num * 2]
-//const numero = array[0] = comentado para testes linha 115***
-//const dobrar = array[1] = comentado para testes linha 115***
-//console.log(dobrar(5)) //array2
+const numero = array[0]               //comentado para testes linha 115***
+const dobrar = array[1]               //comentado para testes linha 115***
+console.log(dobrar(5))                //array2: num => num * 2
 
 // ============= OU
-//const res = dobrar(numero) //array1
-//console.log(res)
+const res = dobrar(numero) //array1: 5
+console.log(res)
 
 //Usando o Destructuring na pratica
-const [numero, dobrar] = array //armazena primeiro e segundo itens do array acima. usando apenas uma linha de codigo
+//const [numero, dobrar] = array //armazena primeiro e segundo itens do array acima. usando apenas uma linha de codigo
 const r = dobrar(numero)
 console.log(r)
 
@@ -144,18 +150,18 @@ console.log(props[proName]) //value 1, acessando o valor de uma propriedade de f
 //Forma tradicional:
 const prop = {pro1: 'value 1'}
 prop.pro2 = 'value 2'
-console.log(prop)
+console.log('forma tradional:', prop)
 
 //Notação de Colchetes
 const newp = 'prop2'
 const p = {pro1: 'value 1', [newp]: 'value 2'}
-console.log(p)
+console.log('notação de colchetes:', p)
 
 //Em uso de parametros
 const gobj = newP => ({prop1: 'value 1', [newP]: 'value 2'})
 const newP = 'prop2'
 const objeto = gobj(newP)
-console.log(objeto)
+console.log('Uso de parametros:', objeto)
 
 
 
--- teste.js
+++ teste.js
@@ -9,3 +9,25 @@ const num = [1, 2, 3]
 num.forEach(() => {
   console.log('Oi') 
 })
+
+
+
+const obj = {
+  prop: 1,
+  prop2: 2,
+  prop3: [1, 2, 3]   //esse array é um objeto aninhado no objeto inicial
+}
+
+const obj2 = {
+  ...obj,                                 //propriedades de obj foram espalhadas ali dentro - Spread
+    prop: 'x',                              // alterou a propriedade de prop
+   prop3: [...obj.prop3, 4]                //False, pq são dois arrays diferentes em memoria
+
+}
+
+obj2.prop3.push(4)
+const obj20 = structuredClone(obj)   
+
+console.log('prop3 do obj2:', obj20.prop3)
+console.log('prop3 do obj:', obj.prop3)
+console.log(obj.prop3 === obj20.prop3) 
